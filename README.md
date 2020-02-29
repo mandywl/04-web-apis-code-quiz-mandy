d23ba75eccd1fc836feff2e8b696fb6a5e5ac9cd
AU\mandy.wells-lakeland <mandy.wellslakeland@akqa.com>
2020-03-01 01:17:31 +1100

    Completed readme and highcores page

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # 04-web-apis-code-quiz-mandy
+
+# Description 
+
+* This is a timed code quiz with multiple-choice questions. This app will run in the browser and feature dynamically updated HTML and CSS powered by JavaScript code. It will also feature a clean and polished user interface and be responsive, ensuring that it adapts to multiple screen sizes.
+
+# ![App Screenshot](appScreenShot.png)
+
+# Deployed url
+
+* https://github.com/mandywl/04-web-apis-code-quiz-mandy
+* https://mandywl.github.io/04-web-apis-code-quiz-mandy/
+
+# Instructions
+
+* Clicking the 'Start Quiz' button to display a series of questions
+
+* Once the quiz begins, a countdown timer starts
+
+* If a question is answered incorrectly, 10 seconds is subtracted from the timer
+
+* The timer ends when all questions have been answered or the timer reaches 0
+
+* After the game ends, user can save their initials and score to view a highscores page
--- script.js
+++ script.js
@@ -3,28 +3,12 @@ var answers = document.querySelector("#answers");
 var timeLeft = 60;
 var page = 0;
 var timeInterval;
+var user = 1;
 
-
-// function timerCountDown() {
-//   if (sessionStorage.getItem("counter")) {
-//     var value = sessionStorage.getItem("counter");
-//   } else {
-//     var value = 60;
-//   }
-//   timerEl.textContent = value + " seconds remaining";
-
-//   var counter = function() {
-//     value = parseInt(value) - 1;
-//     sessionStorage.setItem("counter", value);
-//     timerEl.textContent = value + " seconds remaining";
-//   };
-
-//   var interval = setInterval(counter, 1000);
-// }
+var scoreArray = [];
+var emailArray = [];
 
 function timerCountDown() {
-  var timeLeft = 60;
-
   timeInterval = setInterval(function () {
     timerEl.textContent = timeLeft + " seconds remaining";
     timeLeft--;
@@ -37,36 +21,36 @@ function timerCountDown() {
   }, 1000);
 }
 
-
 timerCountDown();
 
 var questions = [
   {
     questionHeading: "Commonly used data types DO NOT include:",
-    answers: ["1. strings", "2. booleans", "3. alerts", "4. numbers"]
+    answers: ["strings", "booleans", "alerts", "numbers"]
   },
   {
     questionHeading: "Arrays in JavaScript can be used to store_______.",
     answers: [
-      "1. numbers and strings",
-      "2. other arrays",
-      "3. booleans",
-      "4. all of the above"
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "all of the above"
     ]
   },
   {
     questionHeading:
       "String values must be enclosed within_______ when being assigned to variables.",
-    answers: ["1. commas", "2. brackets", "3. quotes", "4. parentheses"]
+    answers: ["commas", "brackets", "quotes", "parentheses"],
+    correctAnswer: "commas"
   },
   {
     questionHeading:
       "A very useful tool used during development and debugging for printing content to the debugger is:",
     answers: [
-      "1. JavasSript",
-      "2. terminal/bash",
-      "3. for loops",
-      "4. console log"
+      "JavasSript",
+      "terminal/bash",
+      "for loops",
+      "console log"
     ]
   }
 ];
@@ -82,21 +66,55 @@ function displayQuestions(page) {
   $(".result").remove("");
 }
 
+function displayScore() {
+  var score = parseInt(localStorage.getItem("timeLeft")) + 1;
+  console.log("score is ", score);
+  $('.quesitonHeading').text("All done");
+  $('#answers').empty();
+  $('h1').after('<p>' +
+    'Your final score is ' + score +
+    '</p>');
+  $('p').after('<p class="msgdiv">' + 'Enter Initials: ' +
+    '<input type="text" name="email" id="email" placeholder="AB" /> ' +
+    '<button id="submit" class="btn btn-primary">Submit</button>' +
+    '</p>');
+  $(".msgdiv").append("<p class='error'></p>")
+
+  $('#submit').on("click", function (event) {
+    event.preventDefault();
+
+    var email = $("#email").val();
+    console.log(email);
+    if (email === "") {
+      $(".error").text("Please enter an Initial")
+    } else {
+      localStorage.setItem("email", email);
+      window.open("highscores.html", "_self");
+    }
+  });
+}
+
+$('#start').on("click", function (event) {
+  localStorage.clear();
+});
+
 $("#answers").on("click", function (event) {
+  var index = event.target.id;
   var element = event.target;
   var result;
-
   if (page < questions.length) {
 
+
     switch (element.innerText) {
-      case "3. alerts":
-      case "2. curly brackets":
-      case "4. all of the above":
-      case "3. quotes":
-      case "4. console log":
+      case "alerts":
+      case "curly brackets":
+      case "all of the above":
+      case "quotes":
+      case "console log":
         result = "Correct";
         break;
       default:
+        timeLeft = timeLeft - 10;
         result = "Wrong";
         break;
     }
@@ -107,19 +125,31 @@ $("#answers").on("click", function (event) {
       page++;
     }, 500);
   } else {
+    localStorage.setItem("timeLeft", timeLeft);
     displayScore();
     clearInterval(timeInterval);
   }
 });
 
-function displayScore() {
-  $('.quesitonHeading').text("All done");
-  $('#answers').remove();
-  $('h1').after('<p>' +
-    'Your final score is' +
-    '</p>');
-  $('p').after('<p>' + 'Enter Initials: ' +
-    '<input type="text" name="email" id="email" placeholder="AB" /> ' +
-    '<button class="btn btn-primary">Submit</button>' +
-    '</p>');
-}
+$(document).ready(function () {
+  scoreArray.push(parseInt(localStorage.getItem("timeLeft")) + 1);
+  emailArray.push(localStorage.getItem("email"));
+  console.log(emailArray[0]);
+  if (emailArray[0] === null) {
+    $('.score').empty();
+    $('.score').append("There is no score at the moment. Please go back to home page.");
+  } else {
+    for (var i = 0; i < scoreArray.length; i++) {
+      console.log(user + ", " + emailArray[i] + " - " + scoreArray[i]);
+      $('.score').empty();
+      $('.score').append(user + ", " + emailArray[i] + " - " + scoreArray[i]);
+    }
+  }
+
+  $('#clear').on("click", function (event) {
+    $('.score').empty();
+    $('.score').append("There is no score at the moment. Please go back to home page.");
+  });
+});
+
+
